b3dd087d28d68c4a43d2e4b22ab8280afe4c539f
shareefevans <shareef3vans@gmail.com>
2024-10-16 08:12:45 +1100

    adding code for historyService

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,15 +1,58 @@
+import fs from 'node:fs/promises';
+
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
+
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    return await fs.readFile('db/searchHistory.json', {
+      flag: 'a+',
+      encoding: 'utf8',
+    });
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    return await fs.writeFile(
+      'db/searchHistory,json',
+      JSON.stringify(cities, null, '\t')
+    );
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[];
+      try {
+        parsedCities = [].concat(JSON.parse(cities));
+      } catch (err) {
+        parsedCities = [];
+      }
+      return parsedCities;
+    });
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+    if (!city) {
+      throw new Error('city can not be blank');
+    }
+    const cities = await this.getCities();
+    if (!cities.find((c) => c.name === city)) {
+      const newCity = new City(city, Date.now().toString());
+      cities.push(newCity);
+      await this.write(cities);
+    }
+    return cities;
+  }
+
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
